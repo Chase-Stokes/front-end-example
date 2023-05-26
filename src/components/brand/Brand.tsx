4d8ee5bee20d516bedbcc0b399e475af64c7ec02
Chase Stokes <chase@sparkblox.com>
2023-05-26 14:45:33 -0700

    complete brand component

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports'
 import './brand.css';
 
-import { google, slack, atlassian, dropbox, shopify } from './imports'
 
 const Brand: React.FC = () => {
   return (
-    <div>
+    <div className='gpt_brand section_padding'>
+      <div>
+        <img src={google} alt='google' />
+      </div>
+      <div>
+        <img src={slack} alt='slack' />
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian' />
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox' />
+      </div>
+      <div>
+        <img src={shopify} alt='shopifyå' />
+      </div>
       Brand
     </div>
   );
