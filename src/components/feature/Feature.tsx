ca6447c03e62fa195b3b3d408612e332660baa7c
Chase Stokes <chase@sparkblox.com>
2023-05-26 16:47:22 -0700

    wrap up gpt and feature components

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './feature.css';
 
-const Feature: React.FC = () => {
+interface FeatureProps {
+  title: string;
+  text: string;
+}
+
+const Feature: React.FC<FeatureProps> = (props) => {
+  const { title, text } = props;
   return (
-    <div>
-      Feature
+    <div className='gpt_features_container_feature'>
+      <div className='gpt_features_container_feature_title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt_features_container_feature_text'>
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
--- src/containers/gpt/Gpt.tsx
+++ src/containers/gpt/Gpt.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './gpt.css';
 
 const Gpt: React.FC = () => {
   return (
-    <div>
-      GPT
+    <div className='gpt_whatgpt section_margin' id='gpt'>
+      <div className='gpt_whatgpt_feature'>
+        <Feature title='What is Chat GPT' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' /> 
+      </div>
+      <div className='gpt_whatgpt_heading'>
+        <h1 className='gradient_text'>The possibilities are beyond your imagination</h1>
+        <p>Explore the library</p>
+      </div>
+      <div className='gpt_whatgpt_container'>
+      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      </div>
     </div>
   );
 }
