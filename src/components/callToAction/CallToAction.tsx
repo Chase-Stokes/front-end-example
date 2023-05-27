1b7fbe8955b016bf025402d8607e0afc564b3fb7
Chase Stokes <chase@sparkblox.com>
2023-05-26 17:45:54 -0700

    wrap up cta component

--- src/components/callToAction/CallToAction.tsx
+++ src/components/callToAction/CallToAction.tsx
@@ -3,8 +3,14 @@ import './callToAction.css';
 
 const CallToAction: React.FC = () => {
   return (
-    <div>
-      CallToAction
+    <div className='gpt_cta'>
+      <div className='gpt_cta_content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring</h3>
+      </div>
+      <div className='gpt_cta_button'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   );
 }
