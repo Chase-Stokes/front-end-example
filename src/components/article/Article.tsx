a3bba0c6b313832b2afedb13e9819d827d6c9e89
Chase Stokes <chase@sparkblox.com>
2023-05-27 17:37:08 -0700

    wrap up article and blog components

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './article.css';
 
-const Article: React.FC = () => {
+interface IArticleProps {
+  imgUrl: string;
+  date?: string;
+  title?: string;
+}
+
+const Article: React.FC<IArticleProps> = (props) => {
+  const { imgUrl, date, title } = props;
+
   return (
-    <div>
+    <div className='gpt_blog_container_article'>
+      <div className='gpt_blog_container_article_image'>
+        <img src={imgUrl} alt='blog pic'/>
+      </div>
+      <div className='gpt_blog_container_article_content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
       Article
     </div>
   );
--- src/containers/blog/Blog.tsx
+++ src/containers/blog/Blog.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
 const Blog: React.FC = () => {
   return (
-    <div>
-      Blog
+    <div className='gpt_blog section_padding' id='blog'>
+      <div className='gpt_blog_heading'>
+        <h1 className='gradient_text'>A lot is happening, 
+We are blogging about it.</h1>
+      </div>
+      <div className='gpt_blog_container'>
+        <div className='gpt_blog_container_groupA'>
+          <Article imgUrl={blog01} date='Aug 28, 2021' title='Exploring ChatGPTs Capabilities' />
+        </div>
+        <div className='gpt_blog_container_groupB'>
+          <Article imgUrl={blog02} date='Aug 28, 2021' title='Unleashing the Power of ChatGPT' />
+          <Article imgUrl={blog03} date='Aug 28, 2021' title='ChatGPT: Revolutionizing AI' />
+          <Article imgUrl={blog04} date='Aug 28, 2021' title='Deep Dive into ChatGPT' />
+          <Article imgUrl={blog05} date='Aug 28, 2021' title='ChatGPT: Shaping the Future' />
+        </div>
+      </div>
     </div>
   );
 }
